b7e04d352ac65dcfa3eaaa10a32901a24f843323
Tom Counsell <contact@tomcounsell.com>
2013-03-17 03:46:52 +0000

    edit seller/assets forms and functions

--- seller/controller/account.py
+++ seller/controller/account.py
@@ -4,7 +4,7 @@ from admin.controller.decorator import access_required
 
 @access_required('seller')
 def home(request, context={}):
-  return render(request, 'seller/home.html')
+  return render(request, 'seller/account/home.html')
 
 def create(account_id):
   try:
@@ -19,53 +19,87 @@ def create(account_id):
 @access_required('seller')
 def edit(request):
   from seller.models import Seller
-  from seller.controller.forms import SellerEditForm
+  from seller.controller.forms import SellerEditForm, AssetProductForm, AssetArtisanForm, AssetToolForm, AssetMaterialForm
+  from django.forms.formsets import formset_factory
+
+  AssetProductFormSet   = formset_factory(AssetProductForm, extra=20,
+                                          can_order=True, can_delete=True)
+  AssetArtisanFormSet   = formset_factory(AssetArtisanForm, extra=20,
+                                          can_order=True, can_delete=True)
+  AssetToolFormSet      = formset_factory(AssetToolForm, extra=20,
+                                          can_order=True, can_delete=True)
+  AssetMaterialFormSet  = formset_factory(AssetMaterialForm, extra=20,
+                                          can_order=True, can_delete=True)
+  #use order fields as the rank number
+
 
   if request.method == 'POST':
-    form = SellerEditForm(request.POST)
-    if form.is_valid():
-      data = form.cleaned_data
-      try: # it must be a post to work
-        seller = Seller.objects.get(pk=request.session['seller_pk'])
+    seller_form       = SellerEditForm(       request.POST)
+    product_formset   = AssetProductFormSet(  request.POST, request.FILES, prefix='product')
+    artisan_formset   = AssetArtisanFormSet(  request.POST, request.FILES, prefix='artisan')
+    tool_formset      = AssetToolFormSet(     request.POST, request.FILES, prefix='tool')
+    material_formset  = AssetMaterialFormSet( request.POST, request.FILES, prefix='material')
+
+    try: # it must be a post to work
+      if seller_form.is_valid():
+        seller_data = seller_form.cleaned_data
+        seller = Seller.objects.get(id=request.session['seller_id'])
         seller.update(data)
         seller.save()
-        return HttpResponseRedirect('/seller/home/')
 
-      except Exception as e:
-        context = {'exception': e}
+      if product_formset.has_changed() and product_formset.is_valid():
+        product_data = product_formset.cleaned_data
+        product_asset = Asset.objects.get_or_create(
+          id    = request.session['seller_pk'],
+          ilk   = 'product',
+          rank  = product_data['ORDER']
+        )
+        product_asset.update(data)
+        product_asset.save()
+      else:
+        context = {'problem': "product asset data invalid"}
 
-  else:
-    form = SellerEditForm()
-    form.fields['country'].widget.attrs['disabled'] = True
-    form.fields['currency'].widget.attrs['disabled'] = True
+      #repeated for each asset ilk
 
-  context = {'form': form}
+        return HttpResponseRedirect('/seller/account/home/')
+
+    except Exception as e:
+      context = {'exception': e}
+
+  else: #not POST
+    seller_form       = SellerEditForm()
+    if 'admin_id' not in request.session:
+      form.fields['country'].widget.attrs['disabled'] = True
+      form.fields['currency'].widget.attrs['disabled'] = True
+
+    product_formset   = AssetProductFormSet(  prefix='product')
+    artisan_formset   = AssetArtisanFormSet(  prefix='artisan')
+    tool_formset      = AssetToolFormSet(     prefix='tool')
+    material_formset  = AssetMaterialFormSet( prefix='material')
+
+  context = {
+              'seller_form':      seller_form,
+              'product_formset':  product_formset,
+              'artisan_formset':  artisan_formset,
+              'tool_formset':     tool_formset,
+              'material_formset': material_formset,
+            }
   return render(request, 'seller/account/edit.html', context)
 
 @access_required('seller')
 def asset(request): # use api.jquery.com/jQuery.post/
   from seller.models import Asset
   from admin.models import Category
-  from seller.controller.forms import SellerAssetForm
+  from seller.controller.forms import AssetProductForm
+  from django.forms.formsets import formset_factory
+
   try: # it must be a post to work
-    form = SellerAssetForm(request.POST)
-    form['seller'] = request.session['seller_pk']
-    if form.is_valid():
-      asset = Asset.objects.get_or_create(**form.cleaned_data)
-
-      """ #I think modelForm will take care of this for us.
-      if ilk == "product" and 'category' in request.POST:
-        category_string = request.POST['category']
-        #category string is a string of space separated category names
-        category_object_list = []
-        for category_name in category_string.split():
-          #split() created a list of strings, one for each category name
-          category_object = Category.object.get(name=category_name)
-          category_object_list.append(category_object)
-
-        asset.category.add(*category_object_list)
-      """
-      asset.save()
+    AssetProductFormset = formset_factory(AssetProductForm)
+    formset = AssetProductForm(request.POST, request.FILES)
+    if formset.is_valid():
+      #asset = Asset.objects.get_or_create(**form.cleaned_data)
+      #if ilk...
+      #asset.save()
       context = {'sucess': True}
     else:
       context = {'problem': "invalid form data"}
--- seller/controller/forms.py
+++ seller/controller/forms.py
@@ -1,4 +1,6 @@
+from django.db import models
 from django import forms
+from seller.models import Asset, Product, Seller
 
 class SellerEditForm(forms.Form):
   from admin.models import Country, Currency
@@ -18,15 +20,48 @@ class SellerEditForm(forms.Form):
     else:
       return error
 
-class SellerAssetForm(forms.ModelForm):
-  class Meta:
-    model = Asset
-    exclude = ('seller',)
+class AssetProductForm(forms.Form):
+  ilk         = forms.CharField(type='hidden', default='product')
+  image       = forms.CharField(type='hidden')#stores the id of the uploaded image
+  name        = forms.CharField(max_length=50, required=False)
+  description = forms.Textarea(required=False)
+  category    = forms.SelectMultiple(required=False,
+                                     choices=({'carpet': "Carpet/Rug"})#category model
+                                    )
+
+class AssetArtisanForm(forms.Form):
+  ilk         = forms.CharField(type='hidden', default='artisan')
+  image       = forms.CharField(type='hidden')#stores the id of the uploaded image
+  name        = forms.CharField(max_length=50, required=False)
+  description = forms.Textarea(required=False)
+
+class AssetToolForm(forms.Form):
+  ilk         = forms.CharField(type='hidden', default='tool')
+  image       = forms.CharField(type='hidden')#stores the id of the uploaded image
+  name        = forms.CharField(max_length=50, required=False)
+  description = forms.Textarea(required=False)
+
+class AssetMaterialForm(forms.Form):
+  ilk         = forms.CharField(type='hidden', default='material')
+  image       = forms.CharField(type='hidden')#stores the id of the uploaded image
+  name        = forms.CharField(max_length=50, required=False)
+  description = forms.Textarea(required=False)
+
+#attempt at a generic Asset Form
+class AssetForm(forms.Form, ilk):
+  ilk         = forms.CharField(type='hidden', default=ilk)
+  image       = forms.CharField(type='hidden')#stores the id of the uploaded image
+  name        = forms.CharField(max_length=50, required=False)
+  description = forms.Textarea(required=False)
+  if ilk == 'product':
+    category  = forms.SelectMultiple(required=False,
+                  choices=({'carpet': "Carpet/Rug"})#category model
+                )
 
 class ImageForm(forms.Form):#a form for posting directly to S3
-  #action   = http://anou.s3.amazonaws.com/
-  file      = forms.FileField()
-  key       = forms.CharField()
-  acl       = forms.CharField(default='public-read', editable=False)
-  policy    = forms.CharField(default='POLICY', editable=False)
-  signature = forms.CharField(defualt='SIGNATURE', editable=False)
+  action      = "http://anou.s3.amazonaws.com/"
+  file        = forms.FileField()
+  key         = forms.CharField()
+  #acl        = forms.CharField(type='hidden', default='public-read', editable=False)
+  #policy     = forms.CharField(default='POLICY', editable=False)
+  #signature  = forms.CharField(default='SIGNATURE', editable=False)
