6181883c2dc340af58d655f76d4d8362b1ee504c
Tom Counsell <contact@tomcounsell.com>
2013-03-17 03:46:20 +0000

    url keywords for seller urls

--- seller/urls.py
+++ seller/urls.py
@@ -3,14 +3,14 @@ from controller import account, product
 
 urlpatterns = patterns('',
 
-  url(r'^$', account.home, name='seller home'), #seller dashboard
-  url(r'^edit$', account.edit, name='seller edit'), #edit seller info
-  url(r'^asset$', account.asset, name='seller asset'), #create/update asset via ajax
+  url(r'^$', account.home, name='home'), #seller dashboard
+  url(r'^edit$', account.edit, name='edit'), #edit seller info
+  url(r'^asset$', account.asset, name='asset'), #create/update asset via ajax
 
-  url(r'^product$', product.home, name='seller product'), #all products
-  url(r'^product/create$', product.create, name='seller product create'),
-  url(r'^product/(?P<id>\d+)$', product.detail, name='seller product detail'),
-  url(r'^product/(?P<id>\d+)/edit$', product.edit, name='seller product edit'),
-  url(r'^product/(?P<id>\d+)/delete$', product.delete, name='seller product delete'),
+  url(r'^product$', product.home, name='product home'), #all products
+  url(r'^product/create$', product.create, name='product create'),
+  url(r'^product/(?P<id>\d+)$', product.detail, name='product detail'),
+  url(r'^product/(?P<id>\d+)/edit$', product.edit, name='product edit'),
+  url(r'^product/(?P<id>\d+)/delete$', product.delete, name='product delete'),
 
 )
