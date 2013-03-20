b3bda0104d79cdeb4086c3730bfbaae60d3ea2ff
Tom Counsell <contact@tomcounsell.com>
2013-03-20 16:42:45 +0000

    trying out inclusion tags

---
+++ seller/templatetags/image_tags.py
@@ -0,0 +1,24 @@
+from django import template
+register = template.Library()
+
+@register.inclusion_tag('account/image.html')
+def image_upload(seller_id, asset_ilk, asset_rank):
+  from seller.models import Asset, Image
+  #asset = get_or_none(Asset, seller=seller_id, ilk=asset_ilk, rank=asset_rank)
+  try:
+    if asset is not None:
+      image = Image.objects.get(id=asset.image).thumb
+    else:
+      image = None
+
+  except Exception as e:
+    image = None
+
+  return {'image': image}
+
+
+def get_or_none(model, **kwargs):
+  try:
+    return model.objects.get(**kwargs)
+  except model.DoesNotExist:
+    return None
