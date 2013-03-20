4370554dc72b2acbaa3109c1f5c6194c0025bde7
Tom Counsell <contact@tomcounsell.com>
2013-03-20 23:40:27 +0000

    move js to separate file

---
+++ static/javascript/seller/account_edit.js
@@ -0,0 +1,72 @@
+$().ready( function(){
+  //assign bootstrap classes
+  $('button').addClass('btn');
+  $('#asset_tabs').addClass('nav').addClass('nav-tabs');
+  $('#asset_tabs').children('li').first().addClass('active');
+  $('#artisan_tab').trigger('click');
+  $('#title').trigger('click');
+  addAssetForms();
+});
+
+$('.asset_tab').click(function(){
+  //make this tab active
+  $('.asset_tab').removeClass('active');
+  $(this).addClass('active');
+  //hide all asset containers, then show the right one
+  $('.asset_container').hide();
+  asset_ilk = $(this).attr('id').replace('_tab','');
+  $('#'+asset_ilk+'_container').show();
+})
+
+$('#title').click(function(){
+  $('#seller_form').slideToggle();
+});
+
+$('.asset_image').click(function(){
+  $(this).closest('form').find('.hidden_fields > .image_text').attr('value', 'tings');
+  addAssetForms();
+});
+
+
+function addAssetForms(){
+  //use jquery appendTo function to move blank asset forms
+  //to asset containers so there is always at least one fresh one.
+
+  //for each asset container
+  $('.asset_container').each(function(){
+    empty_forms = 0;
+
+    //for each asset form inside the containter
+    $(this).children('.asset').each(function(){
+      //count empty forms
+      image_text_element = $(this).find('form > .hidden_fields > .image_text').first();
+      if (image_text_element.val() == ''){
+
+        empty_forms++;
+      }
+    });
+
+    //if there are no empty forms, add one
+    if (empty_forms == 0){
+      //grab an empty form from the hidden .asset_forms div
+      next_asset_form = $('#asset_forms > .asset').first();
+
+      //move it into the container after the last form in there
+      next_asset_form.appendTo($(this));
+    }
+  });
+}
+
+(function($) {
+    $.fn.closest_descendent = function(filter) {
+        var $found = $(),
+            $currentSet = this; // Current place
+        while ($currentSet.length) {
+            $found = $currentSet.filter(filter);
+            if ($found.length) break;  // At least one match: break loop
+            // Get all children of the current set
+            $currentSet = $currentSet.children();
+        }
+        return $found.first(); // Return first match of the collection
+    }
+})(jQuery);
