14abe1d7a167a0b714099b0417a42922bbe05892
Tom Counsell <contact@tomcounsell.com>
2013-03-22 02:06:06 +0000

    activate image-uploader for each form instance

--- static/javascript/seller/account_edit.js
+++ static/javascript/seller/account_edit.js
@@ -33,27 +33,58 @@ function addAssetForms(){
 
   //for each asset container
   $('.asset_container').each(function(){
-    empty_forms = 0;
+    num_forms = 0;
+    num_empty_forms = 0;
 
     //for each asset form inside the containter
     $(this).children('.asset').each(function(){
       //count empty forms
       image_text_element = $(this).find('form > .hidden_fields > .image_text').first();
       if (image_text_element.val() == ''){
-        empty_forms++;
+        num_empty_forms++;
+      }else{
+        num_forms++;
       }
     });
 
     //if there are no empty forms, add one
-    if (empty_forms == 0){
+    if (num_empty_forms == 0){
       //grab an empty form from the hidden .asset_forms div
       new_asset_form = $('#asset_forms > .asset').first().clone(true);
-      //if not product containter, hide category element
-      if ($(this).attr('id').replace('_container','') !== 'product'){
+      //if not product container, hide category element
+      ilk = $(this).attr('id').replace('_container','');
+      if ( ilk !== 'product'){
         new_asset_form.find('.asset_category').hide();
       }
+      //give it a new unique id
+      unique_id = ilk + '_image_' + (num_forms+1).toString();
+      new_asset_form.find('.asset_image').attr('id', unique_id);
       //place it in the container
       new_asset_form.appendTo($(this));
+      initializeFileUploader(unique_id);
     }
+  });
+}
+
+function initializeFileUploader(element_id){
+  csrf_token = $('#csrf_token').attr('value');
+  image_ajax_url = $('#image_ajax_url').attr('value');
+  alert(csrf_token + ' and ' + image_ajax_url + '');
+  var uploader = new qq.FileUploader({
+    action: image_ajax_url,
+    element: $('#'+element_id)[0],
+    multiple: false,
+    onComplete: function(id, fileName, responseJSON) {
+      if(responseJSON.success) {
+        alert("success!");
+      } else {
+        alert("upload failed!");
+      }
+    },
+    params: {
+      'csrf_token': csrf_token,
+      'csrf_name': 'csrfmiddlewaretoken',
+      'csrf_xname': 'X-CSRFToken',
+    },
   });
 }
