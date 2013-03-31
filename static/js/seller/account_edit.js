8497cf68f572a4021dd638d7858bb9982a8e7d3f
Tom Counsell <contact@tomcounsell.com>
2013-03-31 09:27:38 +0000

    autosave asset forms

--- static/js/seller/account_edit.js
+++ static/js/seller/account_edit.js
@@ -1,16 +1,17 @@
 $().ready( function(){
+  COUNTER = 1;
   //assign bootstrap classes
   $('button').addClass('btn');
   $('#asset_tabs').addClass('nav').addClass('nav-tabs');
   $('#asset_tabs').children('li').first().addClass('active');
   $('.file-button').addClass('btn').html('<i class="icon-camera"></i>');
+
+  //run on page load
   $('#artisan_tab').trigger('click');//activate first tab
   $('#title').trigger('click');//get out of the way
+  arrangeAssetForms();
   addAssetForms();
 
-  $('.image-input').each(function(){
-    $(this)
-  });//end .each
 });//end .ready
 
 $('.asset-tab').click(function(){
@@ -27,13 +28,19 @@ $('#title').on('click', function(){
   $('#seller_form').slideToggle();
 });
 
-$('.asset-form form').blur(function(){
-  alert('ajax update!');
-});
+function arrangeAssetForms(){
+  $('#asset_forms .asset').each(function(){
+    //move to proper asset container
+    ilk = $(this).find('.ilk').attr('value');
+    applyDataAndEvents($(this), ilk)
+    if (ilk != ''){
+      $(this).appendTo('#'+ilk+'_container');
+    }
+  });
+}
 
 function addAssetForms(){
-  //use jquery appendTo function to move blank asset forms
-  //to asset containers so there is always at least one fresh one.
+  //copy blank asset forms to asset containers so there is always an extra new one.
 
   //for each asset container
   $('.asset-container').each(function(){
@@ -43,7 +50,7 @@ function addAssetForms(){
     //for each asset form inside the containter
     $(this).children('.asset').each(function(){
       //count empty forms
-      if ($(this).find('#id_image').val() == ''){
+      if ($(this).find('#id_asset_id').val() == ''){
         num_empty_forms++;
       }else{
         num_forms++;
@@ -54,52 +61,36 @@ function addAssetForms(){
     if (num_empty_forms == 0){
 
       //grab an empty form from the hidden .asset_forms div
-      new_asset = $('#asset_forms > .asset').first().clone(true);
+      new_asset = $('#asset_forms .asset').first().clone(true);
 
-      //if not product container, hide category element
       ilk = $(this).attr('id').replace('_container','');
-      if ( ilk !== 'product'){
-        new_asset.find('.asset-category').hide();
-      }
-
-      //give it a new unique id
-      unique_id = ilk + '_image_' + (num_forms+1).toString();
-      image_div = new_asset.find('.image');
-      image_div.attr('id', unique_id);
-
-      //tell form the ilk
-      new_asset.find('#id_ilk').attr('value', ilk)
+      applyDataAndEvents(new_asset, ilk);
 
       //place it in the container
       new_asset.appendTo($(this));
 
-      asset_form = new_asset.find('.image-input');
-      applyFileUploadAction(asset_form, image_div);
     }//end if
   });
 }
 
-function applyFileUploadAction(file_input, image_destination){
-  this_element = file_input;
-  progress_bar = image_destination.siblings('.progress');
-  progress_bar.hide();
+function applyDataAndEvents(asset_form, ilk){
+      //if not product container, hide category element
 
-  file_input.fileupload({
-    dataType: 'json',
-    url: '/seller/ajax/image_save',
+      if ( ilk !== 'product'){
+        asset_form.find('.asset-category').hide();
+      }
 
-    //progressall: function (e, data) {
-    //  var progress = parseInt(data.loaded / data.total * 100, 10);
-    //  progress_bar.find('.bar').css('width', (progress*0.9) + '%');
-    //},
+      //give the image div a new unique id
+      unique_id = ilk + '_image_' + COUNTER.toString();
+      image_div = asset_form.find('.image');
+      image_div.attr('id', unique_id);
 
-    done: function (e, data) {
-      response_data = data['response']();
-      response = response_data.result;
-      image_destination.html('<img src="' + response['thumb_url'] + '">');
-      image_destination.closest('.asset').find('.image-id')
-        .attr('value',response['image_id']);
-      addAssetForms();
-    }
-  });//end fileupload
+      //tell form and autosave elements the ilk
+      asset_form.find('#id_ilk').attr('value', ilk);
+      asset_form.find('.autosave').attr('data-ilk', ilk);
+
+      image_input = asset_form.find('.image-input');
+      applyFileUploadAction(image_input, image_div);
+
+      COUNTER++;
 }
---
+++ static/js/seller/asset.js
@@ -0,0 +1,68 @@
+$().ready( function(){
+  //run on page load
+  image_ajax_url = $('#image_ajax_url').attr('value');
+  asset_ajax_url = $('#asset_ajax_url').attr('value');
+
+  //apply autosave function
+  $('.asset').find('.autosave').autosave({
+    url:asset_ajax_url,
+    before:saveAssetBefore,
+    success:saveAssetSuccess,
+    error:saveAssetError
+  });
+
+});
+
+function saveAssetBefore($this_element){
+  //start 'updating' visual
+  $this_element.closest('.asset').removeClass('error').removeClass('saved').addClass('updating');
+}
+
+function saveAssetSuccess(data,$this_element){
+  //set asset_id data for all elements
+  $this_element.closest('.asset').find('.autosave').attr('data-asset_id',data.asset_id);
+  //finished visual
+  $this_element.closest('.asset').removeClass('updating').addClass('saved');
+
+  //remove if deleted
+  if (data.asset_id == "deleted"){
+    alert("asset has been deleted");
+    $this_element.closest('.asset').fadeOut().remove();
+  }
+  //provide new asset form if necessary
+  addAssetForms();
+}
+
+function saveAssetError(error,$this_element){
+  //error visual
+  $this_element.closest('.asset').removeClass('updating').addClass('error');
+}
+
+function applyFileUploadAction(file_input, image_destination){
+  progress_bar = image_destination.siblings('.progress');
+  progress_bar.hide();
+
+  file_input.fileupload({
+    dataType: 'json',
+    url: image_ajax_url,
+
+    //progressall: function (e, data) {
+    //  var progress = parseInt(data.loaded / data.total * 100, 10);
+    //  progress_bar.find('.bar').css('width', (progress*0.9) + '%');
+    //},
+
+    done: function (e, data) {
+      response_data = data['response']();
+      response = response_data.result;
+      image_destination.html('<img src="' + response['thumb_url'] + '">');
+      image_destination.closest('.asset').find('.image-id')
+        .attr('value',response['image_id']).trigger('change');
+    }
+  });//end fileupload
+}
+
+//https://github.com/cfurrow/jquery.autosave.js
+//example:
+//  $("input").autosave({url:"/save",success:function(){},error:function(){}});
+//
+jQuery.fn.autosave=function(e){function n(e){var n=/^data\-(\w+)$/,r={};r.value=e.value;r.name=e.name;t.each(e.attributes,function(e,t){n.test(t.nodeName)&&(r[n.exec(t.nodeName)[1]]=t.value)});return r}var t=jQuery;t.each(this,function(){var r=t(this),i={data:{},event:"change",success:function(){},error:function(){},before:function(){}};e=t.extend(i,e);var s=n(this),o=s.event||e.event;r.on(o,function(){var r=t(this);s.value=r.val();s=t.extend(s,n(this));var i=s.url?s.url:e.url;e.before&&e.before.call(this,r);t.ajax({url:i,data:s,success:function(t){e.success(t,r)},error:function(t){e.error(t,r)}})})})};
