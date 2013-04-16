c86e421b7d094952cb8bbe3c20a34deadf9336a2
Tom Counsell <contact@tomcounsell.com>
2013-04-16 22:45:14 +0000

    humans and robots

--- anou/urls.py
+++ anou/urls.py
@@ -19,4 +19,7 @@ urlpatterns += patterns('',
 
   # Uncomment the next line to enable the admin:
   # url(r'^admin/', include(admin.site.urls)),
+
+  #url(r'^humans.txt', return_static_file('humans.txt')),
+  #url(r'^robots.txt', return_static_file('robots.txt')),
 )
