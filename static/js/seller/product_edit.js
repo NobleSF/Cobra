f0425d3cd0a5e57ad7bd15cf93741aa68eb7e5fa
Tom Counsell <contact@tomcounsell.com>
2013-04-17 16:37:43 +0000

    edit product template changes

--- static/js/seller/product_edit.js
+++ static/js/seller/product_edit.js
@@ -3,5 +3,9 @@ $().ready( function(){
   $('button').addClass('btn');
 
   //run on page load
+  $('#summary-show-more').bind('click', function(){
+    $('.summary-detail').show();
+    $('#summary-show-more').hide();
+  });
 
 });//end .ready
