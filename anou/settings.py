4a73fe017fa1c202b1f2d829a20a637cdd1f62cb
Tom Counsell <contact@tomcounsell.com>
2013-04-21 11:35:54 +0000

    turn on debug var in templates

--- anou/settings.py
+++ anou/settings.py
@@ -207,7 +207,7 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-  #'django.core.context_processors.debug',
+  'django.core.context_processors.debug',
   'django.core.context_processors.i18n',
   'django.core.context_processors.media',
   'django.core.context_processors.static',
