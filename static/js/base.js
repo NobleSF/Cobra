8b3106e9140b6da1dc217afb563164aa7b0f57ec
Tom Counsell <contact@tomcounsell.com>
2013-04-28 05:30:12 +0100

    update the base template

--- static/js/base.js
+++ static/js/base.js
@@ -1,9 +1,3 @@
 $(function(){//on page load
-  $('#navigation').addClass('nav').addClass('nav-pills').addClass('pull-right')
-  $('#navigation li').addClass('btn-large');
-
-  //Footer
-  $('#footer .container div').addClass('row');
-  $('#footer .container div div').addClass('span'+(12/($('#footer .container div div').length)));
 
 });
