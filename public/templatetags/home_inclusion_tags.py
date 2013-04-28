eadb3d7b050eb83830b9861fa6b342545e7a4794
Tom Counsell <contact@tomcounsell.com>
2013-04-28 05:31:06 +0100

    redesign homepage with custom tag for header

---
+++ public/templatetags/home_inclusion_tags.py
@@ -0,0 +1,15 @@
+from django import template
+register = template.Library()
+
+@register.inclusion_tag('home/home_header.html')
+def home_header_tag(request, style): #style options: full, mini, mobile
+  (full, mini, mobile) = (False, False, False)
+
+  if style == 'full':
+    full = True
+  elif style == 'mini':
+    mini = True
+  elif style == 'mobile':
+    mobile = True
+
+  return {'request':request, 'full': full, 'mini':mini, 'mobile':mobile}
