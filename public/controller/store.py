a1fd050b26ae69952a3d8c8c4db0704eb3b453ef
Tom Counsell <contact@tomcounsell.com>
2013-05-01 00:29:50 +0100

    getting started with store page

--- public/controller/store.py
+++ public/controller/store.py
@@ -4,7 +4,24 @@ from django.core.urlresolvers import reverse
 from django.template import RequestContext
 
 def home(request, seller_id):
-  from seller.models import Seller
-  seller = Seller.objects.get(id=seller_id)
+  from seller.models import Seller, Asset, Product, Photo
 
-  return render(request, 'store/home.html')
+  #try:
+  store = Seller.objects.get(id=seller_id)
+
+  store.assets = Asset.objects.filter(seller=store).order_by('ilk')
+  store.artisans = store.assets.filter(ilk='artisan')
+  store.utilities = store.assets.filter(ilk='tool') | store.assets.filter(ilk='material')
+
+  products = Product.objects.filter(seller=store)
+  for product in products:
+    product.name = product.assets.filter(ilk='product')[0].name
+    product.main_photo = Photo.objects.filter(product=product)[0].thumb
+    product.extra_photos = Photo.objects.filter(product=product)[1:4]
+
+  context = {'store':store, 'products':products}
+
+  #except Exception as e:
+  #  context = {'except':e}
+
+  return render(request, 'store.html', context)
