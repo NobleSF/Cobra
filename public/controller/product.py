681fa2d428773fec8c9ae0e75cb0a1e23c4e8457
Tom Counsell <contact@tomcounsell.com>
2013-05-01 02:25:10 +0100

    getting started with product page

--- public/controller/product.py
+++ public/controller/product.py
@@ -3,10 +3,27 @@ from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
 
-def home(request, id):
-  return render(request, 'product.html',
-    {'id':id}
-  )
+def home(request, product_id):
+  from seller.models import Product, Photo
+
+  try:
+    product = Product.objects.get(id=product_id)
+    seller = product.seller
+
+    product.photos = Photo.objects.filter(product=product).order_by('rank')
+    for photo in product.photos:
+      photo.feature_url = str(photo.thumb).replace('thumb','product')
+
+    artisans  = product.assets.filter(ilk='artisan')
+    tools     = product.assets.filter(ilk='tool')
+    materials = product.assets.filter(ilk='material')
+
+    context = {'product':product}
+
+  except Exception as e:
+    context = {'except':e}
+
+  return render(request, 'product.html', context)
 
 def collection(request, group, name=None):
   return render(request, 'collection.html',
--- public/urls.py
+++ public/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
   url(r'^contact$', home.contact, name='contact'),
 
   # product page at /product/123
-  url(r'^product/(?P<id>\d+)$', product.home, name='product'),
+  url(r'^product/(?P<product_id>\d+)$', product.home, name='product'),
 
   # collection at collection/newest or collection/store/woodshop-brahim
   url(r'^collection/(?P<group>\w+)/?(?P<name>\w+)?$', product.collection, name='collection'),
