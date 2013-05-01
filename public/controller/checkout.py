6c43a833920f7455661e83b12d65597a763cf70e
Tom Counsell <contact@tomcounsell.com>
2013-05-01 20:12:14 +0100

    shopping cart url setup

--- public/controller/checkout.py
+++ public/controller/checkout.py
@@ -1,22 +1,33 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import render, get_object_or_404
-from django.core.urlresolvers import reverse
-from django.template import RequestContext
+from django.shortcuts import render, redirect
+from django.utils import simplejson
 
-def cart(request, username):
-  return render(request, 'checkout/cart.html',
-    {'username':username}
-  )
+def cartAdd(request, product_id):
+  from seller.models import Product
 
-def payment(request, username):
-  return render(request, 'checkout/payment.html',
-    {'username':username}
-  )
+  try:
+    product = Product.objects.get(id=product_id)
 
-def confirmation(request, username):
-  return render(request, 'checkout/confirmation.html',
-    {'username':username}
-  )
+    if 'cart' in request.session:
+      request.session['cart'].products.append(product.id)
+    else:
+      request.session['order_id'] = Cart()
+
+    return redirect('cart')
+
+  except Exception as e:
+    context = {'except':e}
+    return HttpResponseRedirect(request.META["HTTP_REFERER"])
+
+def cart(request, product_id=None):
+
+  return render(request, 'checkout/cart.html')
+
+def payment(request):
+  return render(request, 'checkout/payment.html')
+
+def confirmation(request):
+  return render(request, 'checkout/confirmation.html')
 
 def custom_order(request):
   return render(request, 'checkout/custom_order.html')
--- public/urls.py
+++ public/urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
 
 
   #checkout pages
+  url(r'^checkout/add-to-cart/(?P<product_id>\d+)$', checkout.cartAdd, name='cart-add'),
   url(r'^checkout/cart$', checkout.cart, name='cart'),
   url(r'^checkout/payment$', checkout.payment, name='payment'),
   url(r'^checkout/confirmation$', checkout.confirmation, name='confirmation'),
