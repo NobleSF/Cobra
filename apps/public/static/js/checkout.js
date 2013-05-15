6f2ed1685d1c61b955c4d7254a8e5a5d494df949
Tom Counsell <contact@tomcounsell.com>
2013-05-15 11:13:56 +0200

    notes

--- apps/public/static/js/checkout.js
+++ apps/public/static/js/checkout.js
@@ -11,3 +11,6 @@ $('#checkout').on('click', function(){
   $("#cart-paragraph").removeClass('hidden');
   $("#cart-form").removeClass('hidden');
 });
+
+//use mailcheck on email field
+//https://github.com/kicksend/mailcheck
