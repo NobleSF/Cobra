b523ea7276fa1cba47677cf68baf82a9494b85f2
Tom Counsell <contact@tomcounsell.com>
2013-05-26 00:00:57 +0100

    aw, shit... i really hope this works.

--- settings/settings.py
+++ settings/settings.py
@@ -54,7 +54,7 @@ EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['.herokuapp.com','.theanou.com']
+ALLOWED_HOSTS = ['anou-cobra.herokuapp.com','www.theanou.com','localhost']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
