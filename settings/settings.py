3a5b1b380eb8e8f43a0034a48fc71e5f11c0407e
Tom Counsell <contact@tomcounsell.com>
2013-05-25 23:15:23 +0100

    error pages: maybe this is what we need.

--- settings/settings.py
+++ settings/settings.py
@@ -174,6 +174,7 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 TEMPLATE_DIRS = (
   os.path.join(SITE_ROOT, 'templates'),
+  os.path.join(SITE_ROOT, 'templates/errors'),
   # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
   # Always use forward slashes, even on Windows.
   # Don't forget to use absolute paths, not relative paths.
