2a4fcd5424165f9241b9755d9590004fca884e97
Tom Counsell <contact@tomcounsell.com>
2013-06-18 19:10:59 +0100

    sms functions

--- apps/communication/controller/events.py
+++ apps/communication/controller/events.py
@@ -4,35 +4,87 @@ from apps.communication.controller.sms import sendSMS
 def communicateOrdersCreated(orders):
   try:
     for order in orders:
-      #sendSMS() to seller
-      pass
+      products_string = ""
+      for product in order.products:
+        products_string += str(product.id) + " "
+      address_string = getCustomerAddressFromOrder(order, sms_format=True)
+      msg = products_string + "\r\n" + address_string
+      seller_phone = order.products.all()[0].seller.phone
+      sendSMS(msg, seller_phone)
 
     #send email to buyer
     email = Email('order/created', orders)
     return email.sendTo(getCustomerEmailFromOrder(orders[0]))
+    #returns True or exception string
   except Exception as e:
     return str(e)
 
-def communicateOrderConfirmed(order):
-  #send reply text to seller
-  #send email to buyer
-  return True
+def communicateOrderConfirmed(order, reply_sms_sent=False):
+  try:
+    if not reply_sms_sent:
+      msg = "Shukran"
+      seller_phone = order.products.all()[0].seller.phone
+      sendSMS(msg, seller_phone)
 
-def communicateOrderShipped(order):
-  #send reply text to seller
-  #send email to buyer
-  return True
+    #send email to buyer
+    email = Email('order/confirmed', order)
+    return email.sendTo(getCustomerEmailFromOrder(order))
+    #returns True or exception string
+  except Exception as e:
+    return str(e)
+
+def communicateOrderShipped(order, reply_sms_sent=False):
+  try:
+    if not reply_sms_sent:
+      msg = "Shukran"
+      seller_phone = order.products.all()[0].seller.phone
+      sendSMS(msg, seller_phone)
+
+    #send email to buyer
+    email = Email('order/shipped', order)
+    return email.sendTo(getCustomerEmailFromOrder(order))
+    #returns True or exception string
+  except Exception as e:
+    return str(e)
 
 def communicateOrderSellerPaid(order):
-  #send text to seller
+  try:
+    if not reply_sms_sent:
+      pass
+      #sendSMS() to seller
+
+  except Exception as e:
+    return str(e)
+
+def communicateCustomerSubscribed(order):
   return True
 
+#support functions
 def getCustomerEmailFromOrder(order):
   if order.cart.name:
     return "%s <%s>" % (order.cart.name, order.cart.email)
   else:
     return order.cart.email
 
-def test():
+def getCustomerAddressFromOrder(order, sms_format=False):
+  address  = str(order.cart.name)
+  address += "\r\n" if sms_format else "<br>"
+
+  address += str(order.cart.address1)
+  address += "\r\n" if sms_format else "<br>"
+
+  if order.cart.address2:
+    address += str(order.cart.address1)
+    address += "\r\n" if sms_format else "<br>"
+
+  address += str(order.cart.city) + ","
+  address += str(order.cart.state) + " "
+  address += str(order.cart.postal_code)
+  address += "\r\n" if sms_format else "<br>"
+
+  address += str(order.cart.country)
+  return address
+
+def test_email():
   from django.core.mail import send_mail
   send_mail('Subject here', 'Here is the message.', 'hello@theanou.com', ['dev+test@theanou.com'], fail_silently=False)
--- apps/communication/controller/sms.py
+++ apps/communication/controller/sms.py
@@ -1,12 +1,56 @@
-def sendSMS():
-  pass
+import requests
+from requests.auth import HTTPBasicAuth
+from settings.settings import TELERIVET
+import simplejson as json
 
-#outgoing SMS
-#https://telerivet.com/p/PJ8973e6e346c349cbcdd094fcffa9fcb5/api/rest/sending
+def sendSMS(msg, to_number, priority='1'): #using Telerivet
+  try:
+    post_url =  'https://api.telerivet.com/v1/projects/'
+    post_url += TELERIVET['project_id'] + '/messages/outgoing'
+    authentication = HTTPBasicAuth(TELERIVET['api_key'], TELERIVET['api_key'])
+    parameters = {
+      'phone_id':       TELERIVET['phone_id'],
+      'to_number':      to_number,
+      'content':        msg,
+      'message_type':   'sms',
+      'priority':       priority,
+      'status_url':     TELERIVET['status_url'],
+      'status_secret':  TELERIVET['status_secret']
+    }
+    response = requests.post(post_url, auth=authentication, data=parameters)
+    status_code = response.status_code
+    response_content = json.loads(response.content) #dict of response content
 
-#incoming SMS
-#https://telerivet.com/p/PJ8973e6e346c349cbcdd094fcffa9fcb5/api/webhook/receiving
+  except Exception as e:
+    return str(e)
 
+  else:
+    if status_code == '200':
+      return saveSMS(response_content)
+    else:
+      return False
+
+def saveSMS(sms_content): #takes Telerivet response content detail at
+  #https://telerivet.com/p/PJ8973e6e346c349cbcdd094fcffa9fcb5/api/rest/sending
+  from apps.communication.models import SMS
+  try:
+    sms = SMS(
+      from_number = sms_content['from_number'],
+      to_number   = sms_content['to_number'],
+      message     = sms_content['content'],
+      status      = sms_content['status']
+    )
+    sms.save()
+  except Exception as e:
+    return str(e)
+  else:
+    return True
 
 def incoming():
+  #using telerivet:
+  #https://telerivet.com/p/PJ8973e6e346c349cbcdd094fcffa9fcb5/api/webhook/receiving
+  pass
+
+def status_confirmation():
+  #confirming that a sent SMS successfully reached it's recipient
   pass
