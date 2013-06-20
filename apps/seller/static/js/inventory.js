c7581a5f570de95d1133ff856868a88844bb4309
Tom Counsell <contact@tomcounsell.com>
2013-06-20 22:13:29 +0100

    new icons for seller inventory page

--- apps/seller/static/js/inventory.js
+++ apps/seller/static/js/inventory.js
@@ -3,7 +3,13 @@ $().ready( function(){
   $('.ratings').hide();
 
   $('.product-id').click(function(){
+    $(this).hide();
     $(this).closest('.details').find('.ratings').show();
+  });
+
+  $('.ratings').click(function(){
+    $(this).hide();
+    $(this).closest('.details').find('.product-id').show();
   });
 
 });
