67b13057f8d4f16e6fd2f18b1fd9c4dfc759e9da
Tom Counsell <contact@tomcounsell.com>
2013-06-23 00:01:08 +0100

    edit product cosmetics

--- apps/seller/static/js/orders.js
+++ apps/seller/static/js/orders.js
@@ -3,7 +3,9 @@ $().ready( function(){
   //$('.extra').hide();
 
   $('.product-id').click(function(){
-    $(this).closest('.product').find('.total-cost').show();
+    $(this).closest('.product').find('.costs-detail').hide();
+    $(this).closest('.product').find('.status').hide();
+    $(this).closest('.product').find('.total-cost').toggle();
   });
 
   $('.total-cost').click(function(){
