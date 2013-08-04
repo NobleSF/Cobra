670aaa5038aa20fedf0e3b2bbb206f07473515b8
Tom Counsell <contact@tomcounsell.com>
2013-08-05 00:43:15 +0100

    enable sitemap.xml, humans.txt, robots.txt

--- settings/urls.py
+++ settings/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
 from apps.public.controller import home
 from django.http import HttpResponseRedirect
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
   url(r'^$', home.home, name='home'), #fyi, this is home
   (r'^index.php$', lambda x: HttpResponseRedirect('/')), #and this is home too
 
-  #url(r'^humans.txt', return_static_file('humans.txt')),
-  #url(r'^robots.txt', return_static_file('robots.txt')),
+  (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': {}}),
+  url(r'^humans.txt', direct_to_template, {'template':'humans.txt', 'mimetype':'text/plain'}),
+  url(r'^robots.txt', direct_to_template, {'template':'robots.txt', 'mimetype':'text/plain'}),
 )
 
 urlpatterns += patterns('',
