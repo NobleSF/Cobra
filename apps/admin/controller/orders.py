ca4098caf9d50352198cb9b05e3b5c524df5a7f2
Tom Counsell <contact@tomcounsell.com>
2013-08-28 18:33:54 +0100

    remove old create order placeholder

--- apps/admin/controller/orders.py
+++ apps/admin/controller/orders.py
@@ -7,19 +7,9 @@ from datetime import datetime
 from apps.public.models import Order
 
 @access_required('admin')
-def createOrder(request):
-  return render(request, 'orders/create_order.html', {'form': SMSForm()})
-
 def allOrders(request):
 
-  orders = Order.objects.all().order_by('created_at').reverse()[:20]
+  orders = Order.objects.all().order_by('created_at').reverse()[:30]
 
-
-  context = {
-              'orders': orders
-            }
+  context = {'orders': orders}
   return render(request, 'orders/all_orders.html', context)
-
-
-def allEmail():
-  pass
