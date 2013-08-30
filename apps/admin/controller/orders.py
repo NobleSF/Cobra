c9040c869f16bde2368a360e1030a7fe1d3f9cf0
Tom Counsell <contact@tomcounsell.com>
2013-08-31 00:58:38 +0100

    order details

--- apps/admin/controller/orders.py
+++ apps/admin/controller/orders.py
@@ -8,8 +8,11 @@ from apps.public.models import Order
 
 @access_required('admin')
 def allOrders(request):
+  from apps.communication.controller.order_events import getCustomerAddressFromOrder
 
   orders = Order.objects.all().order_by('created_at').reverse()[:30]
+  for order in orders:
+    order.shipping_address = getCustomerAddressFromOrder(order)
 
   context = {'orders': orders}
   return render(request, 'orders/all_orders.html', context)
