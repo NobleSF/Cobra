9e599d337601427416b545b25a5bf6cc79eb6adf
Tom Counsell <contact@tomcounsell.com>
2013-08-31 12:19:56 +0100

    like it never happened

--- settings/settings.py
+++ settings/settings.py
@@ -155,9 +155,6 @@ ALLOWED_HOSTS = [
   'localhost'
 ]
 
-#from memcacheify import memcacheify
-#CACHES = memcacheify()
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
