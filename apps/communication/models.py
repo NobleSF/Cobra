5834485c13ad94fd3905f781f57398b1d1d62f95
Tom Counsell <contact@tomcounsell.com>
2013-09-03 13:01:40 +0100

    move subscription model to communication app Step 1

--- apps/communication/models.py
+++ apps/communication/models.py
@@ -27,3 +27,8 @@ class SMS(models.Model):
   #update history
   created_at      = models.DateTimeField(auto_now_add = True)
   updated_at      = models.DateTimeField(auto_now = True)
+
+class Subscription(models.Model):
+  email           = models.CharField(max_length=100, unique=True)
+  name            = models.CharField(max_length=100, null=True, blank=True)
+  created_at      = models.DateTimeField(auto_now_add = True)
