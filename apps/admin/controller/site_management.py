e3a698ca27f15aee7da82415019dab4243a6a702
Tom Counsell <contact@tomcounsell.com>
2013-09-11 11:50:25 +0100

    invalidate cache function

--- apps/admin/controller/site_management.py
+++ apps/admin/controller/site_management.py
@@ -5,6 +5,26 @@ from django.contrib import messages
 from django.forms.models import modelformset_factory
 from django.utils import simplejson
 
+
+def invalidate_cache(fragment_name, *vary_on):
+  try:
+    from django.core.cache import cache
+    cache_key = template_cache_key(fragment_name, *vary_on)
+    cache.delete(cache_key)
+  except:
+    return False
+  else:
+    return True
+
+def template_cache_key(fragment_name, *vary_on):
+  # Builds a cache key for a template fragment
+  from django.utils.hashcompat import md5_constructor
+  from django.utils.http import urlquote
+
+  base_cache_key = "template.cache.%s" % fragment_name
+  args = md5_constructor(u":".join([urlquote(var) for var in vary_on]))
+  return "%s.%s" % (base_cache_key, args.hexdigest())
+
 @access_required('admin')
 def country(request):
   from apps.admin.models import Country
