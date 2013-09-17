b82f2230e2ba40e56743a32d461f9f8eaba74ab2
Tom Counsell <contact@tomcounsell.com>
2013-09-17 23:21:54 +0100

    move cache functions to public events controller

---
+++ apps/public/controller/events.py
@@ -0,0 +1,24 @@
+from django.core.cache import cache
+
+def refresh_homepage_cache():
+  invalidate_cache('homepage_header')
+  invalidate_cache('homepage_content')
+  pass
+
+def invalidate_cache(fragment_name, *vary_on):
+  try:
+    cache_key = template_cache_key(fragment_name, *vary_on)
+    cache.delete(cache_key)
+  except:
+    return False
+  else:
+    return True
+
+def template_cache_key(fragment_name, *vary_on):
+  # Builds a cache key for a template fragment
+  from django.utils.hashcompat import md5_constructor
+  from django.utils.http import urlquote
+
+  base_cache_key = "template.cache.%s" % fragment_name
+  args = md5_constructor(u":".join([urlquote(var) for var in vary_on]))
+  return "%s.%s" % (base_cache_key, args.hexdigest())
