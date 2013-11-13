e6ed863e6d19d427edeadb2d4f2f4476c27aa261
Tom Counsell <contact@tomcounsell.com>
2013-11-13 22:55:23 +0000

    implementing sub categories

--- apps/admin/models.py
+++ apps/admin/models.py
@@ -67,11 +67,30 @@ class Color(models.Model):
 class Category(models.Model):
   name          = models.CharField(max_length=50)
   keywords      = models.CharField(max_length=50, blank=True, null=True)
-  parent_category = models.ForeignKey('self', related_name="child_category_set",
+  parent_category = models.ForeignKey('self', related_name='sub_categories',
                                       blank=True, null=True)
+
+  ordering_name = models.CharField(max_length=100)
+
+  @property
+  def is_parent_category(self):
+    return (not self.parent_category)
+
+  def save(self, *args, **kwargs):
+    self.ordering_name = self.get_ordering_name()
+    super(Category, self).save(*args, **kwargs)
+
+  def get_ordering_name(self):
+    if self.parent_category:
+      return u'%s %s' % (unicode(self.parent_category), self.name)
+    else:
+      return self.name
 
   def __unicode__(self):
     return self.name
+
+  class Meta:
+    ordering = ['ordering_name']
 
 class RatingSubject(models.Model):
   #we may be able to move this to a list of choices for the Rating model
