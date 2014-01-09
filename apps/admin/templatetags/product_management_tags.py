bf7896e115eb8c0aa8c3b6f37ce30638fc5df8bc
Tom Counsell <contact@tomcounsell.com>
2014-01-09 17:52:40 +0000

    more cleanup for admin tools

--- apps/admin/templatetags/product_management_tags.py
+++ apps/admin/templatetags/product_management_tags.py
@@ -2,11 +2,12 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('products/product_review_row.html')
-def product_review_row(product):
+def product_review_row(product, admin_type):
   from apps.admin.models import RatingSubject
   rating_subjects = RatingSubject.objects.all()
 
   return {'product': product,
           'rating_subjects':rating_subjects,
-          'rating_values': range(1,6)
+          'rating_values': range(1,6),
+          'admin_type': admin_type
          }
