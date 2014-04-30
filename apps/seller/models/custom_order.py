2145247e196e6b3cd76daada68b5acce709c987b
Tom Counsell <dev@tomcounsell.com>
2014-04-30 10:13:05 +0100

    add model property

--- apps/seller/models/custom_order.py
+++ apps/seller/models/custom_order.py
@@ -18,3 +18,19 @@ class CustomOrder(models.Model):
 
   # MODEL PROPERTIES
   # MODEL FUNCTIONS
+
+
+  @property
+  def display_price_estimate(self):
+
+    old_volume = self.base_product.length * self.base_product.width * self.base_product.height
+    new_volume = self.custom_product.length * self.custom_product.width * self.custom_product.height
+    ratio = float(new_volume)/old_volume
+
+    custom_product.price = base_product.price * ratio
+    custom_product.weight = base_product.weight * ratio
+    #bump estimate to next shipping price tier if close
+    custom_product.weight = (custom_product.weight * 1.05) + 100
+    custom_product.shipping_option.add(base_product.shipping_options.all()[0])
+
+    return int(round(custom_product.display_price))
