566372c6b69cd1e9959856e50241f5c7bc80c30e
Tom Counsell <dev@tomcounsell.com>
2014-05-28 20:13:44 +0200

    controllers for api app

---
+++ apps/api/controllers/listing.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from apps.admin.utils.exception_handling import ExceptionHandler
+from django.views.decorators.cache import cache_page
+import json
+
+def product(request): pass
+"""
+use django-rest to serve public product data
+similar to the product_data feed already in use.
+
+
+
+"""
