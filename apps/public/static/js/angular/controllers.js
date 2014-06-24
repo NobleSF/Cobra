34ecd69d048f9e4098899b815a5c2fa152bc1076
Tom Counsell <dev@tomcounsell.com>
2014-06-24 14:57:48 +0200

    placeholder angular files

---
+++ apps/public/static/js/angular/controllers.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('AnouStore.controllers', [])
+  .controller('HelloController', ['$scope', function($scope) {
+    $scope.name = 'Django Angular Project Template';
+
+  }])
