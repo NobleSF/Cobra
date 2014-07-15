5165ec7baac453e935ddd6882c338a1c3306a21b
Tom Counsell <dev@tomcounsell.com>
2014-07-15 23:31:56 +0300

    define number of pages

--- apps/public/static/js/angular/app.js
+++ apps/public/static/js/angular/app.js
@@ -9,11 +9,14 @@
   app.controller('MarketController', ['$http', function($http){
 
     var market = this;
+    market.pages = 1;
     market.listings = [];
     $http.get('/api/listings/') //todo: load urls in with a constant
     //http://django-angular.readthedocs.org/en/latest/manage-urls.html
     .success(function(data){
-      market.listings = data;
+      var page_size = 24;
+      market.pages = parseInt((data.count+(page_size-1))/page_size);
+      market.listings = data.results;
     });
 
   }]);
