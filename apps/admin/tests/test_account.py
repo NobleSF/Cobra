7d086f86967cd403d21cc4732a7598fae5979779
Tom Counsell <contact@tomcounsell.com>
2014-11-05 14:58:17 -0500

    write some tests for account

---
+++ apps/admin/tests/test_account.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+from apps.admin.models.account import Account
+from apps.admin.controller.account import processPassword
+
+class AccountTest(TestCase):
+  def setUp(self):
+    self.username = "Coop-Milk-Sport"
+    self.email = "test+coop-milk-sport@theanou.com"
+    self.phone = "21212345678"
+    self.password = "1234abcdEFGH!@#$"
+
+    Account.objects.create(
+      username=self.username,
+      email=self.email,
+      phone=self.phone,
+      password=processPassword(self.password)
+    )
+
+  def test_password_encryption(self):
+    """
+      Tests the ability to encrypt passwords
+    """
+    account = Account.objects.get(username=self.username)
+    #encrypted password generated
+    self.assertIsNotNone(account.password)
+    self.assertEqual(processPassword(self.password), account.password)
+    #encrypetd password is not same as original
+    self.assertNotEqual(self.password, account.password)
+
+  def test_create_account(self):
+    """
+    Tests the creation of an account in the DB model
+    """
+    account = Account.objects.get(username=self.username)
+    self.assertIsNotNone(account)
+    self.assertEqual(account.username, self.username)
+    self.assertEqual(account.email, self.email)
+    self.assertEqual(account.phone, self.phone)
+
+  def test_login(self):
+    """
+    Tests the login options for users
+    """
+    pass
