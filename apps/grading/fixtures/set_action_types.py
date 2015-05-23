01705bd6f4e0f1e274e623d28df3a105d3d6d764
Tom Counsell <contact@tomcounsell.com>
2015-05-23 14:22:10 +0700

    create action and action type models

---
+++ apps/grading/fixtures/set_action_types.py
@@ -0,0 +1,8 @@
+from apps.grading.models import ActionType, Action
+
+def create_action_types():
+  for type in ActionType.TYPE_OPTIONS:
+    #create new ActionType
+    #set some defualt parameters
+    #save action_type object
+    pass
